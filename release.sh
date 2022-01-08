82ef143e36eb35085398a1cac921d4892d8066e0
Ashley Gibson <ashley@nosegraze.com>
2022-01-08 16:19:24 +0000

    Make API request.

--- release.sh
+++ release.sh
@@ -10,10 +10,33 @@ if [[ -z "$WORDPRESS_PASS" ]]; then
   exit 1
 fi
 
-RELEASE_URL=$( jq '.url' ${RELEASE_CONFIG} )
-RELEASE_VERSION=$( jq '.version' ${RELEASE_CONFIG} )
-RELEASE_REQUIREMENTS=$( jq '.requirements' ${RELEASE_CONFIG} )
+release_url=$( jq '.url' ${RELEASE_CONFIG} )
+release_version=$( jq '.version' ${RELEASE_CONFIG} )
+release_requirements=$( jq '.requirements' ${RELEASE_CONFIG} )
 
-echo "Version ${RELEASE_VERSION} requirements: ${RELEASE_REQUIREMENTS}"
+echo "Version ${release_version} requirements: ${release_requirements}"
 
-echo "Deploying to ${RELEASE_URL}"
+echo "Deploying to ${release_url}"
+
+response=$(
+curl \
+  -X POST
+  -H "Content-Type: multipart/form-data"
+  --user "${WORDPRESS_USER}:${WORDPRESS_PASS}"
+  -s -w "%{http_code}"
+  -F "file_zip=@${RELEASE_ZIP}"
+  -F "version=${release_version}"
+  -F "file_name=${RELEASE_FILE_NAME}"
+  -F "pre_release=${PRE_RELEASE}"
+  -F "requirements=${release_requirements}"
+  ${release_url}
+)
+
+http_code=$(tail -n1 <<< "$response")
+content=$(sed '$ d' <<< "$response")
+
+if [[ "$http_code" ne 201 ]]; then
+  echo "Invalid response code: ${http_code}"
+  echo "Content: ${content}"
+  exit 1
+fi
