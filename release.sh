6421045f39d21691b2773ea619ca7d2520ee64d7
Ashley Gibson <ashley@nosegraze.com>
2022-01-09 12:49:28 +0000

    Move API URL to secrets.

--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
+# !/usr/bin/bash
+
 # Ensure that the WORDPRESS_USER secret is included
 if [[ -z "$WORDPRESS_USER" ]]; then
   echo "Set the WORDPRESS_USER env variable."
@@ -10,32 +12,42 @@ if [[ -z "$WORDPRESS_PASS" ]]; then
   exit 1
 fi
 
-release_url=$( jq '.url' ${RELEASE_CONFIG} )
-release_version=$( jq '.version' ${RELEASE_CONFIG} )
-release_requirements=$( jq '.requirements' ${RELEASE_CONFIG} )
+# Ensure that the WORDPRESS_RELEASE_URL secret is included
+if [[ -z "$WORDPRESS_RELEASE_URL" ]]; then
+  echo "Set the WORDPRESS_RELEASE_URL env variable."
+  exit 1
+fi
 
-echo "Version ${release_version} requirements: ${release_requirements}"
+if [ -e "$RELEASE_CONFIG" ]; then
+  echo "Parsing requirements from config."
+  release_requirements=$( jq '.requirements' ${RELEASE_CONFIG} )
+else
+  echo "No config found; no requirements to parse."
+  release_requirements=''
+fi
 
-echo "Deploying to ${release_url}"
+echo "Version ${RELEASE_VERSION} requirements: ${release_requirements}"
+
+echo "Deploying ${RELEASE_ZIP} to ${WORDPRESS_RELEASE_URL}"
 
 response=$(
 curl \
-  -X POST
-  -H "Content-Type: multipart/form-data"
-  --user "${WORDPRESS_USER}:${WORDPRESS_PASS}"
-  -s -w "%{http_code}"
-  -F "file_zip=@${RELEASE_ZIP}"
-  -F "version=${release_version}"
-  -F "file_name=${RELEASE_FILE_NAME}"
-  -F "pre_release=${PRE_RELEASE}"
-  -F "requirements=${release_requirements}"
-  ${release_url}
+  -X POST \
+  -H "Content-Type: multipart/form-data" \
+  --user "${WORDPRESS_USER}:${WORDPRESS_PASS}" \
+  -w "%{http_code}" \
+  -F "file_zip=@${RELEASE_ZIP}" \
+  -F "version=${RELEASE_VERSION}" \
+  -F "file_name=${RELEASE_FILE_NAME}" \
+  -F "pre_release=${PRE_RELEASE}" \
+  -F "requirements=${release_requirements}" \
+  "${WORDPRESS_RELEASE_URL}"
 )
 
 http_code=$(tail -n1 <<< "$response")
 content=$(sed '$ d' <<< "$response")
 
-if [[ "$http_code" ne 201 ]]; then
+if [[ $http_code != 201 ]]; then
   echo "Invalid response code: ${http_code}"
   echo "Content: ${content}"
   exit 1
